22d7c803ed9d560802fd39080b3643abda3da0f4
Huajie Yang <huajie@pdx.edu>
2017-08-25 16:24:37 -0700

    Initialize package

---
+++ R/read_data.R
@@ -0,0 +1,13 @@
+#' Reads data in 'file_name' as a data frame
+#'
+#' read data file 'file_name' as a data fraame with external packages
+#' @param file_name name of the file that contains bike counts
+#' @return a data frame with bike counts
+#' @export
+
+read_data <- function(file_name) {
+print(paste(file_name, "is ready to be read into R"))
+  my_df <- readxl::read_excel(file_name)
+  return(my_df)
+  }
+
